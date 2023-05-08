388e98038ba6f8e2c1c1d00d7eb171a46e500081
jelenamijus <93733585+jelenamijus@users.noreply.github.com>
2023-05-08 19:16:14 +0200

    Update update_mysql.sql

--- CDC/update_mysql.sql
+++ CDC/update_mysql.sql
@@ -1,33 +1,18 @@
-CREATE DATABASE IF NOT EXISTS database_name;
-USE database_name;
+CREATE DATABASE IF NOT EXISTS database_datajourney;
+USE database_datajourney;
 
-CREATE TABLE IF NOT EXISTS database_name.example_table (
+CREATE TABLE IF NOT EXISTS database_datajourney.example_table (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-text_col VARCHAR(50),
-int_col INT,
-created_at TIMESTAMP
+event_timestamp INTEGER,
+event_name STRING,
+user_pseudo_id STRING
 );
 
-INSERT INTO database_name.example_table (text_col, int_col, created_at) VALUES
-('abc', 0, '2021-05-01 00:00:00'),
-('def', 1, NULL),
-('ghi', -987, NOW()),
-('jkl', 2786, '2021-05-01 00:00:00'),
-('abc', 0, '2021-05-01 00:00:00'),
-('def', 1, NULL),
-('ghi', -987, NOW()),
-('jkl', 2786, '2021-05-01 00:00:00'),
-('abc', 0, '2021-05-01 00:00:00'),
-('def', 1, NULL),
-('ghi', -987, NOW()),
-('jkl', 2786, '2021-05-01 00:00:00'),
-('abc', 0, '2021-05-01 00:00:00'),
-('def', 1, NULL),
-('ghi', -987, NOW()),
-('jkl', 2786, '2021-05-01 00:00:00'),
-('abc', 0, '2021-05-01 00:00:00'),
-('def', 1, NULL),
-('ghi', -987, NOW()),
-('jkl', 2786, '2021-05-01 00:00:00');
+INSERT INTO database_datajourney.example_table (text_col, int_col, created_at) VALUES
+(1538605647313015, 'level_complete_quickplay', 'D50D60807F5347EB64EF0CD5A3D4C4CD'),
+(1538606005034070, 'level_start_quickplay', 'D50D60807F5347EB64EF0CD5A3D4C4CD'),
+(1538606043777092, 'level_fail_quickplay', 'D50D60807F5347EB64EF0CD5A3D4C4CD'),
+(1538605475353000, 'session_start', 'D50D60807F5347EB64EF0CD5A3D4C4CD'),
+('1538605515191004, 'user_engagement', '2021-05-01 00:00:00');
 
-UPDATE database_name.example_table SET int_col=int_col*2;
+UPDATE database_datajourney.example_table;
