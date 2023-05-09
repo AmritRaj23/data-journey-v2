092e162b449116c39bf3c25adde75f63bb9dc955
jelenamijus <93733585+jelenamijus@users.noreply.github.com>
2023-05-09 21:00:47 +0200

    Update create_mysql.sql

--- CDC/create_mysql.sql
+++ CDC/create_mysql.sql
@@ -2,13 +2,12 @@ CREATE DATABASE IF NOT EXISTS database_datajourney;
 USE database_datajourney;
 
 CREATE TABLE IF NOT EXISTS database_datajourney.example_table (
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-event_timestamp INTEGER,
-event_name STRING,
-user_pseudo_id STRING
+event_timestamp timestamp,
+event_name varchar(255),
+user_pseudo_id varchar(255)
 );
 
-INSERT INTO database_datajourney.example_table (text_col, int_col, created_at) VALUES
-(1538605526387002, 'level_complete_quickplay', 'D50D60807F5347EB64EF0CD5A3D4C4CD'),
-(1538605456440005,'screen_view', 'D50D60807F5347EB64EF0CD5A3D4C4CD'),
-(1538605649314017, 'post_score', '2D50D60807F5347EB64EF0CD5A3D4C4CD');
+INSERT INTO database_datajourney.example_table (event_timestamp, event_name, user_pseudo_id) VALUES
+(153861, 'level_complete_quickplay', 'D50D60807F5347EB64EF0CD5A3D4C4CD'),
+(153862,'screen_view', 'D50D60807F5347EB64EF0CD5A3D4C4CD'),
+(153863, 'post_score', '2D50D60807F5347EB64EF0CD5A3D4C4CD');
