13d3fcaccb3be6b91db5cc603bb0e693255b021b
jelenamijus <93733585+jelenamijus@users.noreply.github.com>
2023-05-09 21:33:34 +0200

    Update update_mysql.sql

--- CDC/update_mysql.sql
+++ CDC/update_mysql.sql
@@ -2,17 +2,14 @@ CREATE DATABASE IF NOT EXISTS database_datajourney;
 USE database_datajourney;
 
 CREATE TABLE IF NOT EXISTS database_datajourney.example_table (
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-event_timestamp INTEGER,
-event_name STRING,
-user_pseudo_id STRING
+event_timestamp timestamp,
+event_name varchar(255),
+user_pseudo_id varchar(255)
 );
 
-INSERT INTO database_datajourney.example_table (text_col, int_col, created_at) VALUES
-(1538605647313015, 'level_complete_quickplay', 'D50D60807F5347EB64EF0CD5A3D4C4CD'),
-(1538606005034070, 'level_start_quickplay', 'D50D60807F5347EB64EF0CD5A3D4C4CD'),
-(1538606043777092, 'level_fail_quickplay', 'D50D60807F5347EB64EF0CD5A3D4C4CD'),
-(1538605475353000, 'session_start', 'D50D60807F5347EB64EF0CD5A3D4C4CD'),
-('1538605515191004, 'user_engagement', '2021-05-01 00:00:00');
-
-UPDATE database_datajourney.example_table;
+INSERT INTO database_datajourney.example_table (event_timestamp, event_name, user_pseudo_id) VALUES
+(153864, 'level_complete_quickplay', 'D50D60807F5347EB64EF0CD5A3D4C4CD'),
+(153865, 'level_start_quickplay', 'D50D60807F5347EB64EF0CD5A3D4C4CD'),
+(153866, 'level_fail_quickplay', 'D50D60807F5347EB64EF0CD5A3D4C4CD'),
+(153867, 'session_start', 'D50D60807F5347EB64EF0CD5A3D4C4CD'),
+(153868, 'user_engagement', 'D50D60807F5347EB64EF0CD5A3D4C4CD');
