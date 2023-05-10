7bfbb96ba96bd916d9e9f638e993014fb4880821
jelenamijus <93733585+jelenamijus@users.noreply.github.com>
2023-05-10 18:33:24 +0200

    Update config_env.sh

--- Data-Simulator/config_env.sh
+++ Data-Simulator/config_env.sh
@@ -2,12 +2,11 @@
 
 export GCP_PROJECT="<PROJECT-ID>"
 export ENDPOINT_URL="<ENDPOINT URL>" # doesn't need to be defined in the very beginning
-# export PUSH_ENDPOINT='<processing-endpoint-url>' # doesn't need to be defined in the very beginning
 export GCP_REGION="<REGION>"
+export DATAFLOW_TEMPLATE=dataflow
 export RUN_PROXY_DIR=cloud-run-pubsub-proxy
-# export RUN_PROCESSING_DIR=processing-service
-# export DATAFLOW_TEMPLATE=beam
-# export RUN_INFERENCE_PROCESSING_SERVICE=inf_processing_service
+# export RUN_PROCESSING_DIR=processing-service     # only needed for /ETL/CloudRun
+# export PUSH_ENDPOINT='<processing-endpoint-url>' # only needed for /ETL/CloudRun
 
 export BUCKET=example-bucket-name-$GCP_PROJECT
 export FILE=sample_events.json
